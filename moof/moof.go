ff04fc3203cde36bbc087dec46f94a65549c356a
David Firestone <165plo@gmail.com>
2014-02-24 18:13:14 -0700

    Added comments lining sections of port
    The comments layout the different Box types that need to be
    implemented.

--- moof/moof.go
+++ moof/moof.go
@@ -1,1 +1,25 @@
 package moof
+
+// Start mfhd
+// End mfhd
+
+// Start tfhd
+// End tfhd
+
+// Start SampleInformation
+// End SampleInformation
+
+// Start trun
+// End trun
+
+// Start traf
+// End traf
+
+// Start box
+// End box
+
+// Start moof
+// End moof
+
+// Start mdat
+// End mdat
