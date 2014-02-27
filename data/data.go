11f6df0688c9076f0aa4233a8de4113a0d820046
Andrew Canastar <andrewcanastar@gmail.com>
2014-02-26 19:05:42 -0700

    added Chris data.go

--- data/data.go
+++ data/data.go
@@ -1,42 +1,62 @@
-/*
-In order to recreate data, it seems like it is necessary to create
-two structs: one for Reader and one for Writer. The entire program
-should run on functions since everything here is like a helper method.
-*/
 package data
 
-// struct for Reader
-type Reader struct {
-	data byte[]
-	cursor int
+import "io/ioutil"
+
+var DEBUG_SIZE int = 100
+
+func Read(fileName string, curser int) []byte {
+
+	b, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		panic(err)
+	}
+
+	return b
 }
 
-// struct for Writer
-type Writer struct {
-	chunks byte[] // unclear from the python
-	size int
+func ReadSegemnt(bytes []byte) uint32 {
+
+	var segment uint32
+
+	l := len(bytes)
+	for i, b := range bytes {
+		shift := uint32((l - i - 1) * 8)
+
+		segment |= uint32(b) << shift
+	}
+	//fmt.Print("segment:", segment)
+
+	return segment
 }
 
-// def read(self, size)
+func ReadBytes(curser int, size int, bytes []byte) []byte {
 
-// def readBytes(self, size)
+	return bytes[curser:(curser + size)]
 
-// def readString(self)
+}
 
-// def readAll(self)
+func TruncateBytes(curser int, bytes []byte) []byte {
 
-// def hasBytes(self)
+	return bytes[curser:len(bytes)]
 
-// def __str__(self)
+}
 
-// def write(self, size, value
+func ReadHeaderData(bytes []byte) uint32 {
 
-// def writeBytes(self, bytes)
+	var curser int = 0
 
-// def writeString(self, s)
+	var a uint32 = (ReadSegemnt(ReadBytes(curser, 1, bytes)) >> 1) & 0x07
+	curser++
+	var b uint32 = ReadSegemnt(ReadBytes(curser, 1, bytes))
+	curser++
+	var c uint32 = (ReadSegemnt(ReadBytes(curser, 1, bytes)) >> 1) & 0x7f
+	curser++
+	var d uint32 = ReadSegemnt(ReadBytes(curser, 1, bytes))
+	curser++
+	var e uint32 = (ReadSegemnt(ReadBytes(curser, 1, bytes)) >> 1) & 0x7f
+	curser++
+	var timestamp uint32 = (a << 30) | (b << 22) | (c << 15) | (d << 7) | e
 
-// def getBytes(self)
+	return timestamp
 
-// def getSize(self)
-
-// def __str__(self)
+}
--- moof/moof_test.go
+++ moof/moof_test.go
@@ -1,1 +1,18 @@
 package moof
+
+import "testing"
+
+func TestRead
+
+/*
+def read(self, data) :
+    cursor = data.cursor
+    size = data.read(4)
+    name = data.read(4)
+    assert name == self.BOXTYPE
+    self.boxes = [ ]
+    while data.cursor - cursor < size :
+      self.boxes.append(Box().read(data))
+    return self
+*/
+
