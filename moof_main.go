f650f4dcb32cb39063d2f09e7fadf13746dcc0fa
David Firestone <165plo@gmail.com>
2014-03-01 10:24:33 -0700

    Cleaned up moof_main.go
    Main was printing the file which is no longer needed. Also removed
    unneeded comment.

--- moof_main.go
+++ moof_main.go
@@ -9,20 +9,8 @@ import(
 )
 
 func main(){
-	/*
-	args:=os.Args
-	file,err := Open(args[1])
-	if(err!=nil){
-		fmt.Println("Unable to load file")
-	}else{
-	fullFile, err := ioutil.ReadFile(file)
-	reader := data.NewReader(fullFile)
-	moof := new(Moof)
-	fmt.Println(moof.Read(reader))	
-	*/
 	args:=os.Args
 	fullFile, err := ioutil.ReadFile(args[1])
-	fmt.Printf("%s",fullFile)
 	if err!=nil{
 		fmt.Println("Error reading file")
 	}else{
