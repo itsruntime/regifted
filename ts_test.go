151143b3c58e1d7c85f1b26653dd953c529f8772
Mathew Guest <mathewguest@users.noreply.github.com>
2014-03-02 07:33:44 -0700

    pat pass only checking headers

--- ts_test.go
+++ ts_test.go
@@ -11,7 +11,41 @@ func awfulStateSetup() {
 }
 
 func TestTransportPacketRead(t *testing.T) {
-	// will endian-ness matter theoretically?
+  // ts header
+  // n_bits value
+	// 8			sync byte is always 'G' 0x47
+	// 1			transport error flag
+	// 1			payload unit start indicator
+	// 1			transport priority indicator
+	// 13			pid
+	// 2			scrambling control
+	// 1			adapaption field exists
+	// 1			contains payload
+	// 4			continuity counter
+	//
+	// bars are byte marks. header is 4 bytes total; i.e. 8 hex chars.
+	//                     |                    |                    |
+  // [sync]                [tei] [psi] [tp] [         pid        ]   [sc] [af] [pf] [cont]
+
+  // bin  	hex
+  // 0000 	0
+  // 0001 	1
+  // 0010 	2
+  // 0011 	3
+  // 0100 	4
+  // 0101 	5
+  // 0110 	6
+  // 0111 	7
+  // 1000 	8
+  // 1001 	9
+  // 1010 	A
+  // 1011 	B
+  // 1100 	C
+  // 1101 	D
+  // 1110 	E
+  // 1111 	F
+
+	// could endian-ness matter theoretically?
 	var packetBytes []byte
 	var packetString string
 
@@ -33,13 +67,10 @@ func TestTransportPacketRead(t *testing.T) {
 	generateBytesFromString(&packetBytes, &packetString)
 	packet := TsPacket{byteChunk: packetBytes}
 	packet.Read()
-	// packet.Print()
-	if packet.sync != 71 { // 71 (hex) = 'G' (ascii)
+	if packet.sync != 0X47 { // 71 = 'G' = 0x47
 		t.Error("Transport Stream Packet read " +
 			"sync byte incorrectly.")
 	}
-	// perhaps read is the wrong word since we don't know what could have
-	// gone wrong
 	if packet.transportError != false {
 		t.Error("Transport Stream Packet read " +
 			"transport error bit incorrectly.")
@@ -47,6 +78,10 @@ func TestTransportPacketRead(t *testing.T) {
 	if packet.unitStart != false {
 		t.Error("Transport Stream Packet read " +
 			"unit start incorrectly.")
+	}
+	if packet.priority != false {
+		t.Error("Transport Stream Packet read " +
+			"priority incorrectly.")
 	}
 	if packet.pid != 610 {
 		t.Error("Transport Stream Packet read " +
@@ -56,47 +91,69 @@ func TestTransportPacketRead(t *testing.T) {
 		t.Error("Transport Stream Packet read " +
 			"scrambling control incorrectly.")
 	}
-	if packet.priority != false {
+	if packet.hasAdaptation != false {
 		t.Error("Transport Stream Packet read " +
-			"priority incorrectly.")
-	}
-	if packet.continuity != 7 {
-		t.Error("Transport Stream Packet read " +
-			"continuity incorrectly.")
+			"adaptation indiciator incorrectly.")
 	}
 	if packet.hasPayload != true {
 		t.Error("Transport Stream Packet read " +
 			"payload indicator incorrectly.")
 	}
+	if packet.continuity != 7 {
+		t.Error("Transport Stream Packet read " +
+			"continuity incorrectly.")
+	}
+
+  packetString = `
+   47 00 01 17 80 70 24 F1 31 D4 33 F3 88 8B 23 5A
+   94 13 D4 1D DD CD 61 D8 73 08 82 88 B1 13 85 BB
+   D0 93 C4 41 54 BE 68 70 70 EA 3D CF 70 1F 50 CD
+   4E 26 A5 00 D2 1C C4 4C EE 71 C4 68 38 2B 91 45
+   44 40 F7 50 85 5D 7F DD 62 58 28 72 D6 4F 39 4A
+   A1 86 83 28 00 9E 2E 01 F0 AE 79 CC 16 F1 3C 19
+   7F FE A3 25 F0 1A 5B 0E CC 45 B6 1C D8 EC 2C E3
+   48 E7 78 9F F1 E1 62 30 78 1C 80 D5 D0 CF DA C0
+   6A A0 01 F4 03 FA AF 80 B2 34 DE 01 DC 14 46 72
+   0F 23 CB 70 21 DE AE 70 1D A0 F1 22 E1 10 79 1C
+   58 EA D4 4F 50 07 D1 3E D8 77 E4 63 65 2C E6 D0
+   9A 11 82 26 CC 62 D6 2E 00 1F DA C3`
+  generateBytesFromString(&packetBytes, &packetString)
+  packet = TsPacket{byteChunk: packetBytes}
+  packet.Read()
+	if packet.transportError != false {
+		t.Error("Transport Stream Packet read " +
+			"transport error bit incorrectly.")
+	}
+	if packet.unitStart != false {
+		t.Error("Transport Stream Packet read " +
+			"unit start incorrectly.")
+	}
+	if packet.priority != false {
+		t.Error("Transport Stream Packet read " +
+			"priority incorrectly.")
+	}
+	if packet.pid != 1 {
+		t.Error("Transport Stream Packet read " +
+			"packet id incorrectly.")
+	}
+	if packet.scramble != 0 {
+		t.Error("Transport Stream Packet read " +
+			"scrambling control incorrectly.")
+	}
 	if packet.hasAdaptation != false {
 		t.Error("Transport Stream Packet read " +
 			"adaptation indiciator incorrectly.")
 	}
+	if packet.hasPayload != true {
+		t.Error("Transport Stream Packet read " +
+			"payload indicator incorrectly.")
+	}
+	if packet.continuity != 7 {
+		t.Error("Transport Stream Packet read " +
+			"continuity incorrectly.")
+	}
 
-  // ts header
-  // | 8*sync | 1*transposrt error flag |  1*payload unit start flag
-  // 13*pid | 2* scrambling control | 1*adaptation field exists | 1*contains payload
-  // 4*continuity counter
-  // packetString = `
-  //  47 00 01 17 80 70 24 F1 31 D4 33 F3 88 8B 23 5A
-  //  94 13 D4 1D DD CD 61 D8 73 08 82 88 B1 13 85 BB
-  //  D0 93 C4 41 54 BE 68 70 70 EA 3D CF 70 1F 50 CD
-  //  4E 26 A5 00 D2 1C C4 4C EE 71 C4 68 38 2B 91 45
-  //  44 40 F7 50 85 5D 7F DD 62 58 28 72 D6 4F 39 4A
-  //  A1 86 83 28 00 9E 2E 01 F0 AE 79 CC 16 F1 3C 19
-  //  7F FE A3 25 F0 1A 5B 0E CC 45 B6 1C D8 EC 2C E3
-  //  48 E7 78 9F F1 E1 62 30 78 1C 80 D5 D0 CF DA C0
-  //  6A A0 01 F4 03 FA AF 80 B2 34 DE 01 DC 14 46 72
-  //  0F 23 CB 70 21 DE AE 70 1D A0 F1 22 E1 10 79 1C
-  //  58 EA D4 4F 50 07 D1 3E D8 77 E4 63 65 2C E6 D0
-  //  9A 11 82 26 CC 62 D6 2E 00 1F DA C3`
-  // generateBytesFromString(&packetBytes, &packetString)
-  // packet = TsPacket{byteChunk: packetBytes}
-  // packet.Read()
-  //  packet.Print()
-
-  //Init()
-
+	// PAT
   fmt.Println( "\n\ntest #2:\n" )
   packetString = `
   4740 0010 0000 b00d 0001 c100 0000 01f0
@@ -114,9 +171,38 @@ func TestTransportPacketRead(t *testing.T) {
   generateBytesFromString(&packetBytes, &packetString)
   packet = TsPacket{byteChunk: packetBytes}
   packet.Read()
-  // packet.Print()
-
-  // t.Error( "force stdout print" )
+  if packet.transportError != false {
+		t.Error("Transport Stream Packet read " +
+			"transport error bit incorrectly.")
+	}
+	if packet.unitStart != true {
+		t.Error("Transport Stream Packet read " +
+			"unit start incorrectly.")
+	}
+	if packet.priority != false {
+		t.Error("Transport Stream Packet read " +
+			"priority incorrectly.")
+	}
+	if packet.pid != 0 {
+		t.Error("Transport Stream Packet read " +
+			"packet id incorrectly.")
+	}
+	if packet.scramble != 0 {
+		t.Error("Transport Stream Packet read " +
+			"scrambling control incorrectly.")
+	}
+	if packet.hasAdaptation != false {
+		t.Error("Transport Stream Packet read " +
+			"adaptation indiciator incorrectly.")
+	}
+	if packet.hasPayload != true {
+		t.Error("Transport Stream Packet read " +
+			"payload indicator incorrectly.")
+	}
+	if packet.continuity != 0 {
+		t.Error("Transport Stream Packet read " +
+			"continuity incorrectly.")
+	}
 }
 
 func TestInit(t *testing.T) {
