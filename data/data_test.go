5c880f8947e601ff8d3f69879a0488ab0c14948e
Mathew Guest <mathewguest@users.noreply.github.com>
2014-03-06 23:11:55 -0700

    removed comments

--- data/data_test.go
+++ data/data_test.go
@@ -154,31 +154,3 @@ func TestReaderReadBytes(t *testing.T) {
 		t.Errorf("ReadBytes(6): got %s; want %s", bytes_, expect)
 	}
 }
-
-// struct for Reader
-
-// struct for Writer
-
-// def read(self, size)
-
-// def readBytes(self, size)
-
-// def readString(self)
-
-// def readAll(self)
-
-// def hasBytes(self)
-
-// def __str__(self)
-
-// def write(self, size, value
-
-// def writeBytes(self, bytes)
-
-// def writeString(self, s)
-
-// def getBytes(self)
-
-// def getSize(self)
-
-// def __str__(self)
