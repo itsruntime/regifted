6ee021188c2bf66514964654365a89ebfd52afd3
lhalver1 <lhalvey09@gmail.com>
2014-03-08 14:22:06 -0700

    nal.go is finished, working on the test file to confirm.

--- nal.go
+++ nal.go
@@ -1,12 +1,13 @@
+
 package main
 
-//import "strings"
-//import "bytes"
-//import bufio
+import "bytes"
 
 type Nal struct {
-	streamType uint32
-	units      []byte
+
+	streamType		uint32
+	units			[]byte
+	
 }
 
 func (n *Nal) read(bytes []byte) {
@@ -16,31 +17,32 @@ func (n *Nal) read(bytes []byte) {
 	if n.streamType == 0x1b {
 		n.readVideo(bytes)
 	} else {
-		n.readAudio(bytes)
+	    n.readAudio(bytes)
 	}
-
+	
 }
 
 func (n *Nal) readVideo(byteArr []byte) {
 
-	var i int = 0
-	//var ba []byte
+	//Looking for this sequence in byteArr
+	sep := []byte{0, 0, 0, 1}
 
-	//items := byteArr //bytes.Split(byteArr, "0\0\0\1", 0)	//*** ??? ***
+	//item is a [][]byte and to append to units we need byte type
+	for _, item := range bytes.Split(byteArr, sep) {
 
-	for ; i < len(byteArr); i++ {
-		value := byteArr[i]
+		if len(item) > 0 {
 
-		ba := []byte{value}
-
-		n.units = append(n.units, ba...)
-
-	}
-
-}
+			//get each byte from item and append it to units
+			for i := 0; i <= len(item) ; i++ {
+				n.units = append(n.units, item[i])
+			}//end if
+			
+		}//end if
+	}//end for
+}//end func
 
 func (n *Nal) readAudio(bytes []byte) {
 
 	n.units = append(n.units, bytes...)
-
+	
 }
