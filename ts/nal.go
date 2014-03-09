6730a2a58e1ccf124af61ce736bc1a51161044e9
lhalver1 <lhalvey09@gmail.com>
2014-03-09 15:28:39 -0600

    Tested the readVideo...Passed!

--- ts/nal.go
+++ ts/nal.go
@@ -1,7 +1,7 @@
-
 package nal
 
 import "bytes"
+//import "fmt"
 
 type Nal struct {
 
@@ -33,7 +33,7 @@ func (n *Nal) readVideo(byteArr []byte) {
 		if len(item) > 0 {
 
 			//get each byte from item and append it to units
-			for i := 0; i <= len(item) ; i++ {
+			for i := 0; i < len(item) ; i++ {
 				n.units = append(n.units, item[i])
 			}//end for
 			
@@ -46,3 +46,31 @@ func (n *Nal) readAudio(bytes []byte) {
 	n.units = append(n.units, bytes...)
 	
 }
+
+/* FOR TESTING SAKE
+  func main() {
+	var units = []byte{}
+	in := []byte{2, 5, 6, 3, 4, 5, 6, 5, 4, 3, 4, 0, 0, 0, 1, 8, 8, 4, 6, 5, 8, 3, 9, 2, 0, 4, 9, 5, 8, 0, 0, 0, 1, 9, 8, 2, 7, 4, 5, 2, 7, 4, 8, 9, 3}
+	
+	//SHOULD BE THE RESULT OF THE PRINT OUT
+	//  out := []byte{ 2, 5, 6, 3, 4, 5, 6, 5, 4, 3, 4, 8, 8, 4, 6, 5, 8, 3, 9, 2, 0, 4, 9, 5, 8, 9, 8, 2, 7, 4, 5, 2, 7, 4, 8, 9, 3 }
+
+
+	//Looking for this sequence in byteArr
+	sep := []byte{0, 0, 0, 1}
+
+	//item is a [][]byte and to append to units we need byte type
+	for _, item := range bytes.Split(in, sep) {
+
+		if len(item) > 0 {
+
+			//get each byte from item and append it to units
+			for i := 0; i < len(item) ; i++ {
+				units = append(units, item[i])
+			}//end for
+			
+		}//end if
+	}//end for
+
+	fmt.Println(units)
+}*/
