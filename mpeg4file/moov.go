c832746510d42f6cd3a805b10c229319a0ac95c4
David Firestone <165plo@gmail.com>
2014-03-14 01:16:47 -0600

    added more to moov.go
    Added constructors to moov. added largeSize for when the box is larger
    then 32 bytes. added setters for each size variable

--- mpeg4file/moov.go
+++ mpeg4file/moov.go
@@ -3,5 +3,30 @@ package moov
 type Moov struct {
 	//extends Box
 	size		uint32
+	largeSize 	uint64
 	boxtype		uint32
+}
+
+func NewMoov (s uint32, box uint32) *Moov{
+	newMoov:=new(Moov)
+	newMoov.size = s
+	newMoov.boxtype = box
+	return newMoov
+}
+
+func NewMoovWithLargeSize (s uint64, box uint32) *Moov{
+	newMoov:=new(Moov)
+	newMoov.size = 1
+	newMoov.largeSize = s
+	newMoov.boxtype = box
+	return newMoov
+}
+
+func (moov *Moov) SetSize (s uint32){
+	moov.size = s
+}
+
+func (moov *Moov) SetLargeSize (s uint64){
+	moov.size = 1
+	moov.largeSize = s
 }
