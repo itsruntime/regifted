6e4a03a25a9e7715dc0446c39295f234a93d2263
David Firestone <165plo@gmail.com>
2014-03-15 00:26:24 -0600

    added methods to mfhd
    Added information to mfhd to be able to build to be part of MoofLevel2
    interface.

---
+++ mpeg4file/moof/mfhd.go
@@ -0,0 +1,38 @@
+package moof
+
+import "strconv"
+
+type MoofLevel2 interface{
+	String() string
+}
+
+type mfhd struct{
+	size uint32
+	largeSize uint64
+	boxType uint32
+	version uint8
+	flags [3]byte
+}
+
+func NewMfhd(s uint64, box uint32, ver uint8, flag [3]byte){
+	newMfhd:=new(mfhd)
+	newMfhd.SetSize()
+	newMfhd.boxType=box
+	newMfhd.version=ver
+	newMfhd.flags=flag
+}
+
+func (m* mfhd) SetSize(s uint64){
+	if s == 0 {
+		m.size = 0
+	} else if s > 4294967295{
+		m.size = 1
+		m.largeSize = s
+	} else {
+		m.size = s
+	}
+}
+
+func (m *mfhd) String() string{
+	return strconv.FormatUint(uint64(m.size),10)
+}
