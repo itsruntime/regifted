43b4673aeb9c7ae1818757f79a42136aefc38a2d
David Firestone <165plo@gmail.com>
2014-04-07 21:14:39 -0600

    Updated traf
    Updated trap to have a write method

--- mpeg4file/moof/traf.go
+++ mpeg4file/moof/traf.go
@@ -33,14 +33,17 @@ func (t *traf) String() string{
 }
 
 func (m *traf) Write() []byte{
-	var data []byte
+	buf := new(bytes.Buffer)
+	var err error
 	// Size
-	if m.size!=1{
-		data = strconv.AppendUint(data, uint64(m.size), 2)	
-	} else {
-		data = strconv.AppendUint(data, m.largeSize, 2)
-	}	
+	err=binary.Write(buf, binary.BigEndian, m.size)
+	if err!=nil{
+		fmt.Println("binary.Write failed:", err)
+	}
 	// BoxType
-	// Contained boxes write
-	return data
+	err = binary.Write(buf,binary.BigEndian,m.boxType)
+	if err!=nil{
+		fmt.Println("binary.Write failed:", err)
+	}
+	return buf.Bytes()
 }
