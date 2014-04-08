5e81ae8d3caa1fedf7f7a0872390713cee8dbf46
Mathew Guest <mathewguest@users.noreply.github.com>
2014-04-08 14:51:23 -0600

    split readers into sep imp files

---
+++ data/base.go
@@ -0,0 +1,10 @@
+package data
+
+import (
+	"regifted/util/mylog"
+)
+
+const LOGGER_NAME = "reader"
+const LOGGER_SEVERITY_LEVEL = mylog.SEV_TRACE
+
+var logger mylog.Logger
--- data/reader.go
+++ data/reader.go
@@ -3,22 +3,12 @@ package data
 import (
 	"regifted/util/mylog"
 
-	"bufio"
-	"fmt"
-	"io"
 	"log"
 	"os"
 	"unsafe"
 )
 
-const LOGGER_NAME = "reader"
-const LOGGER_SEVERITY_LEVEL = mylog.SEV_TRACE
-
-var logger mylog.Logger
-
 // const DEBUG_SIZE int = 100
-
-const BUFFER_SIZE int = 4096
 
 type Reader struct {
 	data   []byte
@@ -29,13 +19,6 @@ type Reader struct {
 type Writer struct {
 	chunks []byte
 	size   int
-}
-
-type BufferedReader struct {
-	buffReader *bufio.Reader
-	buff       []byte
-	Cursor     int64
-	Size       int64
 }
 
 // Creates a new Reader for reading the data from the byte array.
@@ -68,96 +51,6 @@ func NewReaderFromStream(fh *os.File) *Reader {
 	return NewReader(bytes)
 }
 
-func NewBufferedReaderFromStream(fh *os.File) *BufferedReader {
-	// todo( mathew guest ) find a place for logger instantiation
-	logger = mylog.CreateLogger(LOGGER_NAME)
-	logger.SetSeverityThresh(LOGGER_SEVERITY_LEVEL)
-
-	stat, err := fh.Stat()
-	if err != nil {
-		log.Fatal(err)
-	}
-	size := stat.Size()
-
-	// buffer the stream
-	buffReader := bufio.NewReader(fh)
-	reader := new(BufferedReader)
-	reader.buffReader = buffReader
-	reader.buff = make([]byte, BUFFER_SIZE)
-	reader.Size = size
-	return reader
-}
-
-func (reader *BufferedReader) ReadBytes(size uint) []byte {
-	logger.Trace("reader.ReadBytes(%u)", size)
-	// n, err := reader.buffReader.Read(reader.buff)
-	// if err != nil && err != io.EOF { panic(err) }
-	// if n == 0 { fmt.Println ("n==0") }
-
-	returnBuffer := make([]byte, size)
-	n, err := reader.buffReader.Read(returnBuffer)
-	if err != nil && err != io.EOF {
-		panic(err)
-	}
-	if n == 0 {
-		fmt.Println("n==0")
-		return nil
-	}
-	if n != int(size) {
-		buff := make([]byte, n, n)
-		copy(buff, returnBuffer)
-		returnBuffer = buff
-		logger.Critical("%v", len(returnBuffer))
-
-		diff := int(size) - n
-		buff2 := make([]byte, diff)
-		n, err := reader.buffReader.Read(buff2)
-		if err != nil && err != io.EOF {
-			panic(err)
-		}
-		if n == 0 {
-			fmt.Println("n==0")
-			return nil
-		}
-		concat := append(returnBuffer, buff2...)
-
-		logger.Critical("%v", concat)
-		return concat
-	}
-	logger.Trace("num bytes read: %i", n)
-
-	reader.Cursor += int64(size)
-
-	// fmt.Println("READ:")
-	// fmt.Println(returnBuffer)
-	return returnBuffer
-}
-
-func (reader *BufferedReader) Read(size uint) uint {
-	logger.Trace("reader.Read(%u)", size)
-	// buff := make([]byte, size)
-	data := reader.ReadBytes(size)
-	var idx uint = uint(reader.Cursor) + size
-	var idx_max uint = uint(len(data))
-	var idx_ uint
-	if idx <= idx_max {
-		idx_ = idx
-	} else {
-		log.Printf("attempted to read past end of buffer\n")
-		idx_ = idx_max
-	}
-	idx_ -= uint(reader.Cursor)
-
-	var value uint = 0
-	var i uint = 0
-
-	for ; i < idx_; i++ {
-		value |= uint(uint(data[uint64(i)]) << ((idx_ - i - 1) * 8))
-	}
-	// reader.Cursor += int64(idx_)
-	return value
-}
-
 // Reads the number of bytes passed in as size from the data byte array in
 // the Reader struct. It then concatinates the bytes and returns them as a
 // unsigned integer.
@@ -174,8 +67,8 @@ func (r *Reader) Read(size uint) uint {
 		return 0
 	}
 	// if size < 0 {
-	// 	log.Printf("attempted to read <0 bytes\n")
-	// 	return 0
+	//  log.Printf("attempted to read <0 bytes\n")
+	//  return 0
 	// }
 
 	var idx uint = uint(r.Cursor) + size
@@ -209,8 +102,8 @@ func (r *Reader) ReadBytes(size uint64) []byte {
 		return nil
 	}
 	// if size < 0 {
-	// 	log.Printf("attempted to read <0 bytes\n")
-	// 	return 0
+	//  log.Printf("attempted to read <0 bytes\n")
+	//  return 0
 	// }
 
 	var idx uint = uint(r.Cursor) + uint(size)
