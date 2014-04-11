8f83c4b76da07b7d33e93e65c5a86790cdb8ef99
David Firestone <165plo@gmail.com>
2014-04-10 22:24:48 -0600

    Added comments for development
    Added comments about what will need to occur when in the Regift method

--- giftcollection/giftcollection.go
+++ giftcollection/giftcollection.go
@@ -14,6 +14,17 @@ type sample struct{
     flags uint32
 }
 
+//Need a byte array to hold the boxes in a byte 
+//form for printing from the driver later
+//type GiftCollection struct{
+//    FileByte []byte
+//}
+
+// This file could be used to add the ftyp box to the beginning of the 
+// FileByte array. This may not be necessary based on the files provided 
+// by Niell
+//func InitializeFileByte(){}
+
 const (
 AUDIO_STREAM_TYPE = 15
 VIDEO_STREAM_TYPE = 27
@@ -25,6 +36,12 @@ func Regift(tsArray []*ts.TSState) bool{
         videoByte := make([]byte, 0)
         audioSamples := make([]sample, 0)
         videoSamples := make([]sample, 0)
+        // Need a array of boxes to hold the boxes
+        // until they are ready to print
+        // boxes = make([]mpeg4boxes, 0)
+        // IMPORTANT NOTE: To have a array of the boxes they all have to
+        // be in the same interface. I think this means all the box files
+        // will need to be in the same package. 
 
     var audioSize int = 0
     var videoSize int = 0
@@ -53,12 +70,32 @@ func Regift(tsArray []*ts.TSState) bool{
         videoSize = len(videoByte)
 
         videoSamples = append(videoSamples, sample{delta, 0 , 0})
-
-
-
     }
 
+    // Create mdat and add it to boxes array
 
+    // Add audio Samples to boxes array. Append to front of boxes array
+
+    // Add audio trun to boxes array. Append to front of boxes array
+
+    // Add tfhd to boxes array. Append to front of boxes array
+
+    // Add audio traf to boxes array. Append to front of boxes array
+
+    // Add video samples to boxes array. Append to front of boxes array
+
+    // Add video trun to boxes array. Append to front of boxes array
+
+    // Add tfhd to boxes array. Append to front of boxes array
+
+    // Add video traf to boxes array. Append to fron of boxes array
+
+    // Add mfhd to boxes array. Append to front of boxes array
+
+    // Add moof to boxes array. Append to front of boxes array
+
+    // Call the write method for all boxes in boxes array. 
+    // And append the values to the end of the FileByte array.
 
      return false
 
