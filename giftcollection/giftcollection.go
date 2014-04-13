3b4e67d0f713413239d598fee17a6776d058e1d9
Andrew Canastar <andrewcanastar@gmail.com>
2014-04-12 23:41:12 -0600

    started adding the box to giftcollection

--- giftcollection/giftcollection.go
+++ giftcollection/giftcollection.go
@@ -2,10 +2,8 @@ package giftcollection
 
 import (
 	"fmt"
+	"regifted/mp4box"
 	"regifted/ts"
-	//"regifted/mpeg4file"
-	//"regifted/mpeg4file/moof"
-	//"regifted/mpeg4file/moof/traf"
 )
 
 type sample struct {
@@ -48,6 +46,7 @@ func Regift(AccessUnits []*ts.AccessUnit) bool {
 	// IMPORTANT NOTE: To have a array of the boxes they all have to
 	// be in the same interface. I think this means all the box files
 	// will need to be in the same package.
+	Boxes := make([]Box, 0)
 
 	var audioSize int = 0
 	var videoSize int = 0
@@ -70,7 +69,7 @@ func Regift(AccessUnits []*ts.AccessUnit) bool {
 
 		audioSize = len(audioByte)
 
-		audioSamples = append(audioSamples, sample{delta, 0, 0})
+		audioSamples = append(audioSamples, Sample{0, delta, 0})
 
 		// fmt.Println("audioSamples = ", audioSamples)
 
@@ -82,15 +81,36 @@ func Regift(AccessUnits []*ts.AccessUnit) bool {
 
 		videoSize = len(videoByte)
 
-		videoSamples = append(videoSamples, sample{delta, 0, 0})
+		videoSamples = append(videoSamples, Sample{0, delta, 0})
 	}
 
 	// fmt.Println("videoSamples = ", videoSamples)
 
 	// Create mdat and add it to boxes array
-
+	payload := append(videoByte, audioByte...)
+	mdat := Mdat.NewMdat( 
+		uint64(audioSize + videoSize + 8),
+		payload		
+	)
+	
+	Boxes = append(mdat)
+	// Setting Flags for the trun should be done programatically from the
+	// PES data but that can come later
+	audioTrunFlags := make([]byte, 0, 3)
+	audioTrunFlags.append(0x00)
+	audioTrunFlags.append(0x0B)
+	audioTrunFlags.append(0x01)
 	// Add audio Samples to boxes array. Append to front of boxes array
-
+	audioTrun := Trun.NewTrun(
+		0, //size is calculated later
+		0, //version will be zero until we have a reason to do otherwise
+		trunFlags,
+		0, //dataoffset = MOOF.SIZE + 8, must be calculated later
+		0, //no reason for first-sample-flags
+		uint32(len(audioSamples)),
+		audioSamples		
+	)
+	Boxes = append(audioTrun)
 	// Add audio trun to boxes array. Append to front of boxes array
 
 	// Add tfhd to boxes array. Append to front of boxes array
--- mp4boxes/trun.go
+++ mp4boxes/trun.go
@@ -11,26 +11,38 @@ type Trun struct {
 	*FullBoxFields
 	SampleCount uint32
 	//optional fields
-	dataOffset      int32
-	firstSampleFlag uint32
-	samples         []sample
+	DataOffset      int32
+	FirstSampleFlag uint32
+	Samples         []Sample
 }
 
 type Sample struct {
-	SampleDuration                    uint32
-	SampleSize                        uint32
-	SampleFlags                       uint32
-	SampleCompositionTimeOffset       uint32 //when version is 0
-	SampleCompositionTimeOffsetNormal int32  // when version is 1
+	SampleDuration              uint32
+	SampleSize                  uint32
+	SampleFlags                 uint32
+	SampleCompositionTimeOffset uint32 //when version is 0
+	//SampleCompositionTimeOffsetNormal int32  // when version is 1
 }
 
-func NewTrun(s uint32, ver uint32, flag [3]byte, count uint32) *Trun {
+func NewTrun(s uint32, ver uint32, flag [3]byte, doff int,
+	first uint32, count uint32, samples []Sample) *Trun {
 	newTrun := &Tfhd{&FullBoxFields{Size: s,
 		BoxType: 0x7472756E,
 		Version: ver,
 		Flags:   flag},
-		SampleCount: count}
+		DataOffset:      doff,
+		FirstSampleFlag: first,
+		SampleCount:     count,
+		Samples:         samples}
 	return newTrun
+}
+
+func NewSample(d uint32, s uint32, f uint32, scto uint32) *Sample {
+	newSample := &Sample{SampleDuration: d,
+		SampleSize:                  s,
+		SampleFlags:                 f,
+		SampleCompositionTimeOffset: scto}
+	return newSample
 }
 
 func (t *Trun) SetSize(s uint32) {
@@ -66,48 +78,50 @@ func (t *Trun) Write() []byte {
 		fmt.Println("binary.Write failed:", err)
 	}
 	// data offset
-	if m.dataOffset != 0 {
+	if t.dataOffset != 0 {
 		err = binary.Write(buf, binary.BigEndian, t.dataOffset)
 		if err != nil {
 			fmt.Println("binary.Write failed:", err)
 		}
 	}
 	// first sample flags
-	if m.firstSampleFlag != 0 {
+	if t.firstSampleFlag != 0 {
 		err = binary.Write(buf, binary.BigEndian, t.firstSampleFlag)
 		if err != nil {
 			fmt.Println("binary.Write failed:", err)
 		}
 	}
-	for i := 0; i < len(m.samples)-1; i++ {
-		if m.samples[i].sampleDuration != 0 {
-			err = binary.Write(buf, binary.BigEndian, t.samples[i].sampleDuration)
+	for i := 0; i < len(t.Samples)-1; i++ {
+		if t.Samples[i].SampleDuration != 0 {
+			err = binary.Write(buf, binary.BigEndian, t.Samples[i].SampleDuration)
 			if err != nil {
 				fmt.Println("binary.Write failed:", err)
 			}
 		}
-		if m.samples[i].sampleSize != 0 {
-			err = binary.Write(buf, binary.BigEndian, m.samples[i].sampleSize)
+		if t.Samples[i].SampleSize != 0 {
+			err = binary.Write(buf, binary.BigEndian, t.Samples[i].SampleSize)
 			if err != nil {
 				fmt.Println("binary.Write failed:", err)
 			}
 		}
-		if m.samples[i].sampleFlags != 0 {
-			err = binary.Write(buf, binary.BigEndian, m.samples[i].sampleFlags)
+		if t.Samples[i].SampleFlags != 0 {
+			err = binary.Write(buf, binary.BigEndian, t.Samples[i].SampleFlags)
 			if err != nil {
 				fmt.Println("binary.Write failed:", err)
 			}
 		}
 		if m.version == 0 {
-			if m.samples[i].sampleDuration != 0 {
-				err = binary.Write(buf, binary.BigEndian, m.samples[i].sampleCompositionTimeOffset)
+			if t.Samples[i].SampleDuration != 0 {
+				err = binary.Write(buf, binary.BigEndian,
+					t.Samples[i].SampleCompositionTimeOffset)
 				if err != nil {
 					fmt.Println("binary.Write failed:", err)
 				}
 			}
 		} else {
-			if m.samples[i].sampleDuration != 0 {
-				err = binary.Write(buf, binary.BigEndian, m.samples[i].sampleCompositionTimeOffsetNormal)
+			if t.Samples[i].SampleDuration != 0 {
+				err = binary.Write(buf, binary.BigEndian,
+					t.Samples[i].SampleCompositionTimeOffsetNormal)
 				if err != nil {
 					fmt.Println("binary.Write failed:", err)
 				}
